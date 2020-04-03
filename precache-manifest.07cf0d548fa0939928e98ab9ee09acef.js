a479e8ebf4c8341357fec1a5bf2e2be9f14fc370
niuchunlei <niuchunlei@mokahr.com>
2020-04-03 15:07:19 +0800

    博客测试

---
+++ precache-manifest.07cf0d548fa0939928e98ab9ee09acef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb33451bab79b09435bf8502637f781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa8208e0b3d74159a87",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "fe285072f91c15ebd196",
+    "url": "/static/css/main.21b470b2.chunk.css"
+  },
+  {
+    "revision": "eaa8208e0b3d74159a87",
+    "url": "/static/js/2.741449ab.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.741449ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe285072f91c15ebd196",
+    "url": "/static/js/main.65fffcd3.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
