308c7486b705c15c391ae75f6eb00b21eb1ea41f
niuchunlei <niuchunlei@mokahr.com>
2020-04-04 19:52:44 +0800

    换肤功能

--- src/components/comp-nav/index.js
+++ src/components/comp-nav/index.js
@@ -9,12 +9,22 @@ class CompNav extends HTMLElement {
                     <a target="_blank" href="html5_meter.asp">关于我</a>
                     <a target="_blank" href="http://github.com/natureStory">GitHub</a>
                 </div>
+                <div class="skin-btn-box">
+                    <div class="kite-line default-primary-color"></div>
+                    <div class="kite">
+                        <div class="default-primary-color"></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </div>
             </nav>
         `;
     }
 
     constructor() {
         super();
+        this._totalSkinNum = 7;
         this.innerHTML = CompNav.template;
     }
 
@@ -24,11 +34,36 @@ class CompNav extends HTMLElement {
 
     connectedCallback() {
         document.addEventListener("scroll", this.render);
+        document.querySelector('#comp-nav .kite').addEventListener("click", this.kiteClick);
     }
 
     disconnectedCallback() {
         document.removeEventListener("scroll", this.render);
+        document.removeEventListener("click", this.kiteClick);
     }
+
+    kiteClick = () => {
+        document.querySelector('#comp-nav .skin-btn-box').className = "skin-btn-box active";
+        this.changeSkin();
+        setTimeout(() => {
+            document.querySelector('#comp-nav .skin-btn-box').className = "skin-btn-box";
+        }, 500);
+    };
+
+    changeSkin = () => {
+        const skinNum = window.localStorage.getItem('skinNum');
+        console.log(skinNum);
+        let tempSkinNum = `${Math.ceil(Math.random() * this._totalSkinNum)}`;
+        if (skinNum === tempSkinNum) {
+            if (skinNum< this._totalSkinNum) {
+                tempSkinNum = `${parseInt(tempSkinNum) + 1}`
+            } else {
+                tempSkinNum = `${parseInt(tempSkinNum) - 1}`
+            }
+        }
+        window.localStorage.setItem('skinNum', tempSkinNum);
+        document.body.className = `skin${tempSkinNum}`;
+    };
 
     render = () => {
         if (this.scrollTop > 300) {
