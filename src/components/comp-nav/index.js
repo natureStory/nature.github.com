15e5ea2a881f7ce1c9ec6e2dffa7ef0c3b379800
niuchunlei <niuchunlei@mokahr.com>
2020-04-05 22:23:32 +0800

    优化体验

--- src/components/comp-nav/index.js
+++ src/components/comp-nav/index.js
@@ -6,7 +6,7 @@ class CompNav extends HTMLElement {
                 <span class="blog-title secondary-text-color">我的博客</span>
                 <div class="header-nav-list">
                     <a href="/">主页</a>
-                    <a target="_blank" href="/src/pages/aboutme.html">关于我</a>
+                    <a href="/src/pages/aboutme.html">关于我</a>
                     <a target="_blank" href="http://github.com/natureStory">GitHub</a>
                 </div>
                 <div class="skin-btn-box">
