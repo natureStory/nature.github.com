a2621d05e6a3c10a706633ba2d4251a18769c3de
niuchunlei <niuchunlei@mokahr.com>
2020-04-08 23:25:08 +0800

    加入加载

--- src/components/comp-list/index.js
+++ src/components/comp-list/index.js
@@ -2,6 +2,16 @@ class CompList extends HTMLElement {
     static get template() {
         return `
             <div id="comp-list">
+                <div id="loading-style">
+                    <div id="loading-center">
+                        <div id="loading-center-absolute">
+                            <div class="object default-primary-color" id="object-one"></div>
+                            <div class="object default-primary-color" id="object-two"></div>
+                            <div class="object default-primary-color" id="object-three"></div>
+                            <div class="object default-primary-color" id="object-four"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         `;
     }
